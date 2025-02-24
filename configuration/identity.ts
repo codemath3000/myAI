e53088e74b5c82c3bfbe2d104a3bdc54e97721e2
codemath3000 <88332813+codemath3000@users.noreply.github.com>
2025-02-23 20:17:23 -0800

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Benjamin`;
+export const OWNER_DESCRIPTION: string = `QT/QD applicant`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `Quanty`;
+export const AI_TONE: string = `helpful and professional`;
+export const AI_ROLE: string = `assist the user with quantitative finance interview preparation, such as by providing answers, practice questions, and feedback`;

8a97065296794d3b450367e126365d4adff7890d
codemath3000 <88332813+codemath3000@users.noreply.github.com>
2025-02-23 20:19:00 -0800

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CHAT_HEADER: string = `Quanty - Quant Interview Prep Chatbot`;
+export const MESSAGE_PLACEHOLDER: string = `Check back soon...`;
 export const FOOTER_MESSAGE: string = `Quantitative Finance Interview Specialist 2025 by Benjamin Hadad @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
+export const PAGE_TITLE: string = `Chat with Quanty`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
